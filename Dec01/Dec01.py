c22785d1667c36e676262a71d1705e5f91fb8a74
F1scherman <brayden.f.jonsson@gmail.com>
2023-11-30 22:47:19 -0700

    Day 1 complete

--- Dec01/Dec01.py
+++ Dec01/Dec01.py
@@ -1,3 +1,75 @@
 # Brayden Jonsson, 2023
 
+file = open("challenge_text.txt", "r")
+# sumOfValues = 0
+#
+# for line in file:
+#     num = 0
+#     for firstCharIndex in range(len(line)):
+#         if line[firstCharIndex].isdigit():
+#             num += int(line[firstCharIndex]) * 10
+#             break
+#     for lastCharIndex in range(len(line) - 1, -1, -1):
+#         if line[lastCharIndex].isdigit():
+#             num += int(line[lastCharIndex])
+#             break
+#
+#     sumOfValues += num
+# print(sumOfValues)
 
+sumOfValues = 0
+
+mapNameToNum = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+for line in file:
+    smallestIndex = 999999999999
+    smallestIndexValue = 0
+    largestIndex = -1
+    largestIndexValue = 0
+    for key in mapNameToNum.keys():
+        index = 0
+        try:
+            index = line.index(key)
+        except ValueError:
+            continue
+        if index < smallestIndex:
+            smallestIndex = index
+            smallestIndexValue = mapNameToNum[key]
+
+    for key in mapNameToNum.keys():
+        index = line.rfind(key)
+        if index > largestIndex:
+            largestIndex = index
+            largestIndexValue = mapNameToNum[key]
+
+    for value in mapNameToNum.values():
+        index = 0
+        try:
+            index = line.index(str(value))
+        except ValueError:
+            continue
+        if index < smallestIndex:
+            smallestIndex = index
+            smallestIndexValue = value
+
+    for value in mapNameToNum.values():
+        index = line.rfind(str(value))
+        if index > largestIndex:
+            largestIndex = index
+            largestIndexValue = value
+
+    sumOfValues += largestIndexValue + smallestIndexValue * 10
+
+print(sumOfValues)
+
+file.close()
