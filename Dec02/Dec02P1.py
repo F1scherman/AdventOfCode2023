2c197632290b82ac0ebf4bedd9a18c97751fef5b
F1scherman <brayden.f.jonsson@gmail.com>
2023-12-01 22:31:27 -0700

    Cleaned up a little

--- Dec02/Dec02P1.py
+++ Dec02/Dec02P1.py
@@ -2,47 +2,49 @@
 
 file = open("challenge_text.txt", "r")
 
-sumGame = 0
+sum_game = 0
 
 for line in file:
     id_list = line.split(":")
     i = len(id_list[0]) - 1
     while id_list[0][i].isdigit():
         i -= 1
-    gameId = int(id_list[0][i + 1:])
-    game_list = id_list[1].split(";")
+    game_id = int(id_list[0][i + 1:])
+    round_list = id_list[1].split(";")
 
-    for game in game_list:
-        if "Game" in game:
-            continue
-        red_location = game.find("red")
-        blue_location = game.find("blue")
-        green_location = game.find("green")
+    for game_round in round_list:
+        red_location = game_round.find("red")
+        blue_location = game_round.find("blue")
+        green_location = game_round.find("green")
+
         red_count = 0
         blue_count = 0
         green_count = 0
+
         if red_location != -1:
             i = red_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            red_count = int(game[i + 1:red_location - 1])
+            red_count = int(game_round[i + 1:red_location - 1])
+
         if blue_location != -1:
             i = blue_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            blue_count = int(game[i + 1:blue_location - 1])
+            blue_count = int(game_round[i + 1:blue_location - 1])
+
         if green_location != -1:
             i = green_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            green_count = int(game[i + 1:green_location - 1])
+            green_count = int(game_round[i + 1:green_location - 1])
 
         if red_count > 12 or green_count > 13 or blue_count > 14:
             break
     else:
-        sumGame += gameId
+        sum_game += game_id
 
 
-print(sumGame)
+print(sum_game)
 
 file.close()
--- Dec02/Dec02P2.py
+++ Dec02/Dec02P2.py
@@ -2,43 +2,42 @@
 
 file = open("challenge_text.txt", "r")
 
-sumGame = 0
+sum_game = 0
 
 for line in file:
     id_list = line.split(":")
-    i = len(id_list[0]) - 1
-    while id_list[0][i].isdigit():
-        i -= 1
-    gameId = int(id_list[0][i + 1:])
-    game_list = id_list[1].split(";")
+    round_list = id_list[1].split(";")
 
     red_minimum = 0
     blue_minimum = 0
     green_minimum = 0
-    for game in game_list:
-        if "Game" in game:
-            continue
-        red_location = game.find("red")
-        blue_location = game.find("blue")
-        green_location = game.find("green")
+
+    for game_round in round_list:
+        red_location = game_round.find("red")
+        blue_location = game_round.find("blue")
+        green_location = game_round.find("green")
+
         red_count = 0
         blue_count = 0
         green_count = 0
+
         if red_location != -1:
             i = red_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            red_count = int(game[i + 1:red_location - 1])
+            red_count = int(game_round[i + 1:red_location - 1])
+
         if blue_location != -1:
             i = blue_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            blue_count = int(game[i + 1:blue_location - 1])
+            blue_count = int(game_round[i + 1:blue_location - 1])
+
         if green_location != -1:
             i = green_location - 2
-            while game[i].isdigit():
+            while game_round[i].isdigit():
                 i -= 1
-            green_count = int(game[i + 1:green_location - 1])
+            green_count = int(game_round[i + 1:green_location - 1])
 
         if red_count > red_minimum:
             red_minimum = red_count
@@ -46,8 +45,9 @@ for line in file:
             blue_minimum = blue_count
         if green_count > green_minimum:
             green_minimum = green_count
-    sumGame += red_minimum * blue_minimum * green_minimum
 
-print(sumGame)
+    sum_game += red_minimum * blue_minimum * green_minimum
+
+print(sum_game)
 
 file.close()
