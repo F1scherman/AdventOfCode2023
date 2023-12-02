3cfdcbc010e031b1c310b5db51899d00119e7630
F1scherman <brayden.f.jonsson@gmail.com>
2023-12-01 22:52:41 -0700

    Fixed docs :skull:

--- helper.py
+++ helper.py
@@ -1,3 +1,3 @@
 def extract_int(string):
-    """This extracts an integer from a string. Breaks if multiple valid integers exist"""
+    """This extracts an integer from a string. Functionality breaks if multiple valid integers exist"""
     return int(''.join(filter(lambda a: a.isdigit(), string)))
