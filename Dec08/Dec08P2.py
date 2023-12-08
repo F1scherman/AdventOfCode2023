69cfa008ce2d68b697ff46caf942bbc5ca25392a
BraydenJonsson <brayden.f.jonsson@gmail.com>
2023-12-07 22:39:54 -0700

    This should be the solution

--- Dec08/Dec08P2.py
+++ Dec08/Dec08P2.py
@@ -1,11 +1,15 @@
 # Brayden Jonsson, 2023
 import helper
+import math
 
-def check_if_win(arr):
-    for key in arr:
-        if key[-1] != "Z":
-            return False
-    return True
+# def check_if_win(arr):
+#     for key in arr:
+#         if key[-1] != "Z":
+#             return False
+#     return True
+
+def check_if_win(value: str):
+    return value[-1] == "Z"
 
 file = open("challenge_text.txt", "r")
 sum_of_values = 0
@@ -24,26 +28,30 @@ for line in file:
 
     next_step[key] = (value_1, value_2)
 
-current_steps = []
-for step in next_step.keys():
-    if step[-1] == "A":
-        current_steps.append(step)
+nodes = []
+for node in next_step.keys():
+    if node[-1] == "A":
+        nodes.append(node)
 
-won = False
-while not won:
-    for c in instruction_line:
-        if check_if_win(current_steps):
-            won = True
-            break
-        if c == "R":
-            for i in range(len(current_steps)):
-                current_steps[i] = next_step[current_steps[i]][1]
-        else:
-            for i in range(len(current_steps)):
-                current_steps[i] = next_step[current_steps[i]][0]
-        sum_of_values += 1
+nodes_repeats = []
+for node in nodes:
+    count = 0
+    current_node = node
+    while not check_if_win(current_node):
+        for c in instruction_line:
+            if check_if_win(current_node):
+                break
+            if c == "R":
+                current_node = next_step[current_node][1]
+            else:
+                current_node = next_step[current_node][0]
+            count += 1
 
+    nodes_repeats.append(count)
 
+sum_of_values = 1
+for node_count in nodes_repeats:
+    sum_of_values = math.lcm(sum_of_values, node_count)
 
 print(sum_of_values)
 
