5a41598010ea6bfa3b9e235f6c9ac5b06f32c05f
F1scherman <brayden.f.jonsson@gmail.com>
2023-12-12 14:33:59 -0700

    part 1 done and part 2 set up, but brute force pain

--- Dec12/Dec12P1.py
+++ Dec12/Dec12P1.py
@@ -1,8 +1,6 @@
 # Brayden Jonsson, 2023
 import helper
 
-from itertools import chain
-
 
 def validate_list(arr: [str], nums: [int]):
     current_nums_index = 0
@@ -25,11 +23,15 @@ def validate_list(arr: [str], nums: [int]):
             current_nums_index += 1
         current_arr_index += 1
 
+    if counting:
+        if current_arr_index - start_index != nums[current_nums_index]:
+            return False
+        current_nums_index += 1
     if current_arr_index != len(arr):
         for i in arr[current_arr_index:]:
             if i != ".":
                 return False
-    elif current_nums_index != len(nums) - 1:
+    elif current_nums_index != len(nums):
         return False
     return True
 
@@ -43,10 +45,15 @@ def count_up(arr: [str]):
             arr[i] = "."
 
 
-file = open("sample_text.txt", "r")
+file = open("challenge_text.txt", "r")
 sum_of_values = 0
-
+line_counter = 1
 for line in file:
+    print(f"Line Number: {line_counter}")
+    line_counter += 1
+
+    temp_count = 0
+
     nums = helper.extract_all_ints(line)
 
     # find the ranges of all question mark values
@@ -64,9 +71,10 @@ for line in file:
             unconfirmed_ranges.append(range(start_value, i))
             counting = False
 
-    unconfirmed = unconfirmed_ranges[0]
-    for i in range(1, len(unconfirmed_ranges)):
-        unconfirmed = chain(unconfirmed, unconfirmed_ranges[i]) # DOESN'T WORK
+    unconfirmed = []
+    for unconfirmed_range in unconfirmed_ranges:
+        for i in unconfirmed_range:
+            unconfirmed.append(i)
 
     char_array = [char for char in line if char in ["#", "?", "."]]
 
@@ -77,7 +85,7 @@ for line in file:
         current_index += 1
 
     if validate_list(char_array, nums):
-        sum_of_values += 1
+        temp_count += 1
 
     count_up(insert_array)
 
@@ -87,10 +95,11 @@ for line in file:
             char_array[i] = insert_array[current_index]
             current_index += 1
         if validate_list(char_array, nums):
-            sum_of_values += 1
+            temp_count += 1
         count_up(insert_array)
 
-    print(f"{line}, {sum_of_values}")
+    print(f"This line's count: {temp_count}")
+    sum_of_values += temp_count
 
 print(sum_of_values)
 
--- Dec12/Dec12P2.py
+++ Dec12/Dec12P2.py
@@ -1,48 +1,118 @@
 # Brayden Jonsson, 2023
+import helper
 
-file = open("challenge_text.txt", "r")
+
+def validate_list(arr: [str], nums: [int]):
+    current_nums_index = 0
+    current_arr_index = 0
+    counting = arr[0] == "#"
+    start_index = 0
+    while current_arr_index < len(arr) and current_nums_index < len(nums):
+        if arr[current_arr_index] == "#" and counting:
+            pass
+        elif arr[current_arr_index] != "#" and not counting:
+            pass
+        elif arr[current_arr_index] == "#":
+            start_index = current_arr_index
+            counting = True
+        else:
+            if current_arr_index - start_index != nums[current_nums_index]:
+                return False
+            counting = False
+            start_index = current_arr_index
+            current_nums_index += 1
+        current_arr_index += 1
+
+    if counting:
+        if current_arr_index - start_index != nums[current_nums_index]:
+            return False
+        current_nums_index += 1
+    if current_arr_index != len(arr):
+        for i in arr[current_arr_index:]:
+            if i != ".":
+                return False
+    elif current_nums_index != len(nums):
+        return False
+    return True
+
+
+def count_up(arr: [str]):
+    for i in range(len(arr)):
+        if arr[i] == ".":
+            arr[i] = "#"
+            break
+        else:
+            arr[i] = "."
+
+
+file = open("sample_text.txt", "r")
 sum_of_values = 0
-mapNameToNum = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9
-}
-
+line_counter = 1
 for line in file:
-    smallestIndex = 999999999999
-    smallestIndexValue = 0
-    largestIndex = -1
-    largestIndexValue = 0
-    for key in mapNameToNum.keys():
-        index = line.find(key)
-        if index != -1 and index < smallestIndex:
-            smallestIndex = index
-            smallestIndexValue = mapNameToNum[key]
+    print(f"Line Number: {line_counter}")
+    line_counter += 1
 
-        index = line.rfind(key)
-        if index > largestIndex:
-            largestIndex = index
-            largestIndexValue = mapNameToNum[key]
+    temp_count = 0
 
-    for value in mapNameToNum.values():
-        valueString = str(value)
-        index = line.find(valueString)
-        if index != -1 and index < smallestIndex:
-            smallestIndex = index
-            smallestIndexValue = value
+    original_nums = helper.extract_all_ints(line)
+    nums = []
+    for i in range(5):
+        for num in original_nums:
+            nums.append(num)
 
-        index = line.rfind(valueString)
-        if index > largestIndex:
-            largestIndex = index
-            largestIndexValue = value
+    original_char_array = [char for char in line if char in ["#", "?", "."]]
+    char_array = []
+    for i in range(5):
+        for char in original_char_array:
+            char_array.append(char)
+        if i != 4:
+            char_array.append("?")
 
-    sum_of_values += largestIndexValue + smallestIndexValue * 10
+    char_array_copy_with_delimiter = char_array.copy()
+    char_array_copy_with_delimiter.append("\n")
+
+    # find the ranges of all question mark values
+    unconfirmed_ranges = []
+    start_value = 0
+    counting = line[0] == "?"
+    for i in range(len(char_array_copy_with_delimiter)):
+        char = char_array_copy_with_delimiter[i]
+        if char == "?" and counting:
+            continue
+        elif char == "?":
+            start_value = i
+            counting = True
+        elif counting:
+            unconfirmed_ranges.append(range(start_value, i))
+            counting = False
+
+    unconfirmed = []
+    for unconfirmed_range in unconfirmed_ranges:
+        for i in unconfirmed_range:
+            unconfirmed.append(i)
+
+    insert_array = ["." for i in unconfirmed]
+    current_index = 0
+    for i in unconfirmed:
+        char_array[i] = insert_array[current_index]
+        current_index += 1
+
+    if validate_list(char_array, nums):
+        temp_count += 1
+
+    count_up(insert_array)
+
+    while not helper.is_all_equal_to(insert_array, "."):
+        current_index = 0
+        for i in unconfirmed:
+            char_array[i] = insert_array[current_index]
+            current_index += 1
+        if validate_list(char_array, nums):
+            temp_count += 1
+        count_up(insert_array)
+
+    print(f"This line's count: {temp_count}")
+    sum_of_values += temp_count
 
 print(sum_of_values)
 
